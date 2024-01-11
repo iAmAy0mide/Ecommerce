b8bbd3057fd8dcf5d8a646f81a84083881580512
Ayomide Enirayetan <ayomidekenny952@gmail.com>
2024-01-11 09:45:12 -0800

    added mongo connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,25 @@
 const http = require('http');
 require('dotenv').config();
 
+const { 
+    mongooseConnection,
+} = require('./services/mongo')
+
 const app = require('./app');
 
 const PORT = process.env.PORT
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
+(async function startServer() {
+    try {
+        await mongooseConnection();
+        console.log('MongoDB connected!');
+    } catch (err) {
+        throw new Error(err);
+    }
+
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`);
+    });
+})();
---
+++ server/src/services/mongo.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const MONGO_URI = process.env.MONGO_URI;
+
+async function mongooseConnection() {
+    await mongoose.connect(MONGO_URI, {
+        wtimeoutMS: 500,
+        maxPoolSize: 2500
+    });
+}
+
+module.exports = {
+    mongooseConnection,
+}
